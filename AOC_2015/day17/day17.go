882e32cd7533f048400e42027238c46001f8a321
Toni Le <toni.le@outlook.de>
2020-12-26 19:37:23 +0100

    Day 17 first half

---
+++ AOC_2015/day17/day17.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	capacity = 150
+)
+
+var (
+	containers = []int{}
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		container, err := strconv.Atoi(input)
+		if err != nil {
+			panic(err)
+		}
+		containers = append(containers, container)
+	}
+	sort.Ints(containers)
+	total := amountPossibleCombinations(containers, capacity)
+
+	fmt.Println(total)
+}
+
+func amountPossibleCombinations(containers []int, target int) int {
+	total := 0
+
+	if len(containers) == 2 {
+		if containers[0]+containers[1] == target {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	for i := 1; i < len(containers); i++ {
+		if containers[0]+containers[i] == target {
+			total = total + 1
+		}
+	}
+
+	return total + amountPossibleCombinations(containers[1:], target) + amountPossibleCombinations(containers[1:], target-containers[0])
+}
