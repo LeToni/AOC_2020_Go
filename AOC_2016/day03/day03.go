d969735f37a8caa72c56af2e43310dce23f14d22
Toni Le <toni.le@outlook.de>
2020-12-31 12:45:17 +0100

    day03 first half complete

--- AOC_2016/day03/day03.go
+++ AOC_2016/day03/day03.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type Triangle struct {
 	sideA, sideB, sideC int
 }
@@ -15,5 +21,28 @@ func (triangle *Triangle) IsTriangle() bool {
 }
 
 func main() {
+	file, err := os.Open("input.txt")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
 
+	scanner := bufio.NewScanner(file)
+	count := 0
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		var (
+			a, b, c int
+		)
+		if n, _ := fmt.Sscanf(input, "  %d  %d  %d", &a, &b, &c); n == 3 {
+			triangle := Triangle{a, b, c}
+			if triangle.IsTriangle() {
+				count++
+			}
+		} else {
+			panic(n)
+		}
+	}
+	fmt.Println("Number of triangles found: ", count)
 }
