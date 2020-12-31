af731e0212f71fd65a92181a654940cc799c1fee
Toni Le <toni.le@outlook.de>
2020-12-31 12:10:57 +0100

    Day2 first half finished

--- AOC_2016/day02/day2.go
+++ AOC_2016/day02/day2.go
@@ -2,7 +2,23 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+)
+
+type Position struct {
+	x, y int
+}
+
+var (
+	KeyPad = [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	position   = Position{x: 1, y: 1}
+	directions string
 )
 
 func main() {
@@ -15,10 +31,25 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		directions := scanner.Text()
+		directions = scanner.Text()
 
 		for _, direction := range directions {
-
+			move(direction)
+			// fmt.Println(KeyPad[position.x][position.y])
 		}
+		fmt.Print(KeyPad[position.x][position.y])
+	}
+	fmt.Println("")
+}
+
+func move(direction rune) {
+	if direction == 'U' && !(position.x == 0) {
+		position.x = position.x - 1
+	} else if direction == 'L' && !(position.y == 0) {
+		position.y = position.y - 1
+	} else if direction == 'D' && !(position.x == 2) {
+		position.x = position.x + 1
+	} else if direction == 'R' && !(position.y == 2) {
+		position.y = position.y + 1
 	}
 }
