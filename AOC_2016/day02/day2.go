b439369867f9e9d621879f109e1e3987f938106b
Toni Le <toni.le@outlook.de>
2020-12-31 12:28:17 +0100

    Day02 complete

--- AOC_2016/day02/day2.go
+++ AOC_2016/day02/day2.go
@@ -17,7 +17,16 @@ var (
 		{7, 8, 9},
 	}
 
-	position   = Position{x: 1, y: 1}
+	AdvancedKeyPad = [5][5]byte{
+		{'-', '-', '1', '-', '-'},
+		{'-', '2', '3', '4', '-'},
+		{'5', '6', '7', '8', '9'},
+		{'-', 'A', 'B', 'C', '-'},
+		{'-', '-', '9', '-', '-'},
+	}
+
+	// position   = Position{x: 1, y: 1}
+	position   = Position{x: 0, y: 3}
 	directions string
 )
 
@@ -34,15 +43,17 @@ func main() {
 		directions = scanner.Text()
 
 		for _, direction := range directions {
-			move(direction)
+			// moveOnKeyPad(direction)
+			moveOnAdvancedKeyPad(direction)
 			// fmt.Println(KeyPad[position.x][position.y])
 		}
-		fmt.Print(KeyPad[position.x][position.y])
+		// fmt.Print(KeyPad[position.x][position.y])
+		fmt.Print(string(AdvancedKeyPad[position.x][position.y]))
 	}
 	fmt.Println("")
 }
 
-func move(direction rune) {
+func moveOnKeyPad(direction rune) {
 	if direction == 'U' && !(position.x == 0) {
 		position.x = position.x - 1
 	} else if direction == 'L' && !(position.y == 0) {
@@ -50,6 +61,18 @@ func move(direction rune) {
 	} else if direction == 'D' && !(position.x == 2) {
 		position.x = position.x + 1
 	} else if direction == 'R' && !(position.y == 2) {
+		position.y = position.y + 1
+	}
+}
+
+func moveOnAdvancedKeyPad(direction rune) {
+	if direction == 'U' && !(position.x == 0) && AdvancedKeyPad[position.x-1][position.y] != '-' {
+		position.x = position.x - 1
+	} else if direction == 'L' && !(position.y == 0) && AdvancedKeyPad[position.x][position.y-1] != '-' {
+		position.y = position.y - 1
+	} else if direction == 'D' && !(position.x == len(AdvancedKeyPad)-1) && AdvancedKeyPad[position.x+1][position.y] != '-' {
+		position.x = position.x + 1
+	} else if direction == 'R' && !(position.y == len(AdvancedKeyPad)-1) && AdvancedKeyPad[position.x][position.y+1] != '-' {
 		position.y = position.y + 1
 	}
 }
